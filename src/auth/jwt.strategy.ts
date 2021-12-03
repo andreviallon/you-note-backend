a06ab23245b28863551e47a5a726dc1b2a19c65b
Andre Viallon <viallon.andre@gmail.com>
2021-12-03 17:26:49 +0100

    chore: jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from 'src/auth/interfaces/jwt-payload.interface';
+import { User } from './user.entity';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UsersRepository)
+    private usersRepository: UsersRepository,
+    private configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  public async validate(payload: JwtPayload): Promise<User> {
+    const user: User = await this.usersRepository.findOne({
+      username: payload.username,
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Something went wrong... :(');
+    }
+
+    return user;
+  }
+}
