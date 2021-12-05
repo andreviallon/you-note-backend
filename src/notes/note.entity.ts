b63109ec619002518dc9d58cbf6c5acac7dfa790
Andre Viallon <viallon.andre@gmail.com>
2021-12-05 20:48:48 +0100

    chore: create relation between note and user

--- src/notes/note.entity.ts
+++ src/notes/note.entity.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Note {
@@ -13,4 +15,8 @@ export class Note {
 
   @Column()
   content: string;
+
+  @ManyToOne(() => User, (user) => user.notes, { eager: false })
+  @Exclude({ toPlainOnly: true })
+  user: User;
 }
