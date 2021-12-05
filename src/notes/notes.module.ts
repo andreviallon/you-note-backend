452ab6b635eac3f167f5db254f683365376c0c8b
Andre Viallon <viallon.andre@gmail.com>
2021-12-05 20:49:31 +0100

    chore: register notes repo

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { NotesRepository } from './notes.repository';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([NotesRepository]), AuthModule],
+})
 export class NotesModule {}
