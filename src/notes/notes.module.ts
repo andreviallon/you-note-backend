4f1854856fc0a05da5c66fd38a4467f070b61b5a
Andre Viallon <viallon.andre@gmail.com>
2021-12-06 16:46:19 +0100

    chore: register NotesController

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { NotesController } from './notes.controller';
 import { NotesRepository } from './notes.repository';
 import { NotesService } from './notes.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([NotesRepository]), AuthModule],
   providers: [NotesService],
+  controllers: [NotesController],
 })
 export class NotesModule {}
