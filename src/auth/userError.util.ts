287f3d6af71b8584387d5d81334fe9bb88c143e8
Andre Viallon <viallon.andre@gmail.com>
2021-12-22 13:32:38 +0100

    chore: update error message

--- src/auth/userError.util.ts
+++ src/auth/userError.util.ts
@@ -7,14 +7,14 @@ export class UserError {
   public static handleUserError(error) {
     switch (error.code) {
       case '23505':
-        this.duplicatedName();
+        this.duplicatedEmail();
       default:
         this.internalServerError();
     }
   }
 
-  public static duplicatedName(): string {
-    throw new ConflictException('Username already exists');
+  public static duplicatedEmail(): string {
+    throw new ConflictException('Email already exists');
   }
 
   public static internalServerError(): string {
